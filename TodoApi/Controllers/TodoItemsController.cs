3204bdbc95fc5223e0d65464667cb620af5130a6
wtime <wtime@DESKTOP-D02CN47>
2022-07-04 14:38:37 +0900

    Add project files.

---
+++ TodoApi/Controllers/TodoItemsController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public TodoItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            return await _context.TodoItems.ToListAsync();
+        }
+
+
+        [HttpGet("search-by-name")]
+        public async Task<IActionResult> 
+            GetTodoItemSearch([FromQuery]string name)
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            var todoItems = await _context.TodoItems
+                .Where(x => x.Name.Contains(name))
+                .Select(x => new { Name = x.Name, Completed = x.IsComplete })
+                .ToListAsync();
+
+            if (todoItems == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(todoItems);
+        }
+
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(int id)
+        {
+          if (_context.TodoItems == null)
+          {
+              return NotFound();
+          }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(int id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+          if (_context.TodoItems == null)
+          {
+              return Problem("Entity set 'AppDbContext.TodoItems'  is null.");
+          }
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(int id)
+        {
+            if (_context.TodoItems == null)
+            {
+                return NotFound();
+            }
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoItemExists(int id)
+        {
+            return (_context.TodoItems?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
